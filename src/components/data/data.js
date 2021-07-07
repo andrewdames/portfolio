3c07bc4fefb23ae3b41b31592c900793578cfc97
Andrew Dames <andrewdames@Andrews-Mac-mini.local>
2021-07-07 15:47:15 -0500

    first commit

---
+++ src/components/data/data.js
@@ -0,0 +1,68 @@
+// src/data.js
+
+export const projects = [
+    {
+      title: "Verasol",
+      subtitle: "Craft, Twig, React",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./verasol.gif",
+      link: "https://verasol.org",
+    },
+    {
+      title: "Nationwide Coils",
+      subtitle: "Craft, Twig",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./nationwide.gif",
+      link: "https://nationwidecoils.com",
+    },
+    {
+      title: "Capstone DC",
+      subtitle: "Wordpress",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./project-3.gif",
+      link: "https://capstonedc.com",
+    },
+    {
+      title: "Concentric Ag",
+      subtitle: "Wordpress",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./concentric.gif",
+      link: "https://www.concentricag.com",
+    },
+    {
+      title: "Bantam Bagels",
+      subtitle: "Shopify",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./bantam.gif",
+      link: "https://bantambagels.com",
+    },
+    {
+      title: "One Beat Medical",
+      subtitle: "Shopify",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./onebeat.gif",
+      link: "https://onebeatmedical.com",
+    },
+    {
+      title: "Everlast Climbing",
+      subtitle: "Wordpress",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./everlast.gif",
+      link: "https://everlastclimbing.com",
+    },
+    {
+      title: "Fund Black Businesses",
+      subtitle: "Vue, Craft Headless CMS, GSAP",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./fbb.gif",
+      link: "#",
+    },
+  ];
---
+++ src/data.js
@@ -0,0 +1,100 @@
+// src/data.js
+
+export const projects = [
+  {
+    title: "Verasol",
+    subtitle: "Craft CMS, PHP, JS, Twig, React, Node",
+    description:
+      "A custom built Craft CMS site with a SQL database built using PHP, JS, GSAP for some minor animations and transitions and Twig with a custom tool for comparing energy usage in products using React and Node",
+    image: "./verasol.png",
+    link: "https://verasol.org",
+  },
+  {
+    title: "Nationwide Coils",
+    subtitle: "Craft CMS, PHP, Twig",
+    description:
+      "A custom built Craft CMS site with a SQL database built using PHP, JS and Twig.",
+    image: "./nationwide.png",
+    link: "https://nationwidecoils.com",
+  },
+  {
+    title: "Capstone DC",
+    subtitle: "Wordpress, JS",
+    description:
+      "A Wordpress site with a SQL database built using a custom child theme with customized Gutenburg blocks.",
+    image: "./capstone.png",
+    link: "https://capstonedc.com",
+  },
+  {
+    title: "Concentric Ag",
+    subtitle: "Wordpress",
+    description:
+      "A Wordpress site with a SQL database built using a custom child theme with custom taxonomies and hooks.",
+    image: "./concentric.png",
+    link: "https://www.concentricag.com",
+  },
+  {
+    title: "Bantam Bagels",
+    subtitle: "Shopify, Liquid, JS",
+    description:
+      "A Shopify site built using a custom theme with a lot of JS and custom Liquid compenents.",
+    image: "./bantam.png",
+    link: "https://bantambagels.com",
+  },
+  {
+    title: "One Beat Medical",
+    subtitle: "Shopify",
+    description:
+      "A Shopify site built using a custom theme with a lot of JS and custom Liquid compenents.",
+    image: "./onebeat.png",
+    link: "https://onebeatmedical.com",
+  },
+  {
+    title: "Everlast Climbing",
+    subtitle: "Shopify",
+    description:
+      "A Shopify site built using a customized theme.",
+    image: "./everlast.png",
+    link: "https://everlastclimbing.com",
+  },
+  {
+    title: "Fund Black Businesses",
+    subtitle: "Vue, Craft Headless CMS, GSAP",
+    description:
+      "A custom built Vue site using Craft CMS site as a Headless SQL database built using Vue, JS and GSAP for the transitions and animations. (Unfortunately the site has not been cared for by the client so I just have this GIF showing some of the page transitions)",
+    image: "./fbb.gif",
+    link: "#",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Node",
+  "Vue",
+  "GSAP",
+  "Shopify",
+  "Wordpress",
+  "PHP",
+  "Craft CMS",
+  "Twig",
+  "UX",
+  "UI",
+];
